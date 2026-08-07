0d7333716f180196ea8ff75695da9e1fcf72734b
zenai <zenaigrokritik@gmail.com>
2026-08-07 18:26:42 +0200

    Інтерфейси для сервісів (v.1)

---
+++ services/ICartService.cs
@@ -0,0 +1,11 @@
+﻿// CartService.cs Interface
+
+namespace TradeSpace.Services;
+using TradeSpace.Models;
+
+public interface ICartService
+{
+    Task<Cart> GetCartByUserIdAsync(Guid userId);
+    Task AddToCartAsync(Guid userId, Guid productId, int quantity);
+    Task ClearCartAsync(Guid cartId);
+}
---
+++ services/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿// OrderService.cs Interface
+
+namespace TradeSpace.Services;
+using TradeSpace.Models;
+
+public interface IOrderService
+{
+    Task<Order> CheckoutAsync(Guid userId, string shippingAddress);
+}
